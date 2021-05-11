903b7508394083d62719ecebad0c62cfb7c8a4d6
Matthew Roose <mattr1@vt.edu>
2021-05-11 11:55:22 -0400

    Initial commit

---
+++ src/test/java/spring/supercontest/SupercontestApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.supercontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
