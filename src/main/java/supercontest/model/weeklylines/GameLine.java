809c933f3ddc84af9b7bdc6a4cef6dc719c832f3
Matthew Roose <mattr1@vt.edu>
2021-05-16 15:59:21 -0400

    Added gameId field

--- src/main/java/supercontest/model/weeklylines/GameLine.java
+++ src/main/java/supercontest/model/weeklylines/GameLine.java
@@ -1,11 +1,14 @@
 package supercontest.model.weeklylines;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class GameLine {
 
     // Before the game
+    @Id
+    private String gameId;
     private Team homeTeam;
     private Team awayTeam;
     private float homeTeamHandicap; // 0 if favored
@@ -16,7 +19,8 @@ public class GameLine {
     private Integer homeTeamScore; // null until initialized
     private Integer awayTeamScore; // null until initialized
 
-    public GameLine(Team homeTeam, Team awayTeam, float homeTeamHandicap, float awayTeamHandicap, String gameTime) {
+    public GameLine(String gameId, Team homeTeam, Team awayTeam, float homeTeamHandicap, float awayTeamHandicap, String gameTime) {
+        this.gameId = gameId;
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.homeTeamHandicap = homeTeamHandicap;
