dd5a91c1c3a8a3dc596624d91d9643d087385b5b
Matthew Roose <mattr1@vt.edu>
2021-05-16 16:08:36 -0400

    Simplified GameLine model class

--- src/main/java/supercontest/model/weeklylines/GameLine.java
+++ src/main/java/supercontest/model/weeklylines/GameLine.java
@@ -11,20 +11,18 @@ public class GameLine {
     private String gameId;
     private Team homeTeam;
     private Team awayTeam;
-    private float homeTeamHandicap; // 0 if favored
-    private float awayTeamHandicap; // 0 if favored
+    private float homeTeamHandicap; // positive if underdog, negative if favored
     private String gameTime; // will parse and use as Date where we allow picks to be submitted
 
     // After the game
-    private Integer homeTeamScore; // null until initialized
-    private Integer awayTeamScore; // null until initialized
+    private Integer homeTeamScore; // null until initialized after the game
+    private Integer awayTeamScore; // null until initialized after the game
 
-    public GameLine(String gameId, Team homeTeam, Team awayTeam, float homeTeamHandicap, float awayTeamHandicap, String gameTime) {
+    public GameLine(String gameId, Team homeTeam, Team awayTeam, float homeTeamHandicap, String gameTime) {
         this.gameId = gameId;
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.homeTeamHandicap = homeTeamHandicap;
-        this.awayTeamHandicap = awayTeamHandicap;
         this.gameTime = gameTime;
     }
 }
