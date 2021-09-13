19eff8f7aeeacdc027284e690064dfd680f16c2e
Matthew Roose <mattr1@vt.edu>
2021-09-12 20:58:10 -0400

    Renamed wins and losses fields

--- src/main/java/supercontest/model/player/WeekOfPicks.java
+++ src/main/java/supercontest/model/player/WeekOfPicks.java
@@ -16,33 +16,33 @@ public class WeekOfPicks {
 
     // After the games
     private float weeklyScore;
-    private int wins;
-    private int losses;
-    private int pushes;
+    private int weeklyWins;
+    private int weeklyLosses;
+    private int weeklyPushes;
 
     // Empty week of picks is added to every player when new lines are posted
     public WeekOfPicks(int weekNumber) {
         this.weekNumber = weekNumber;
         this.picks = new ArrayList<>();
         this.weeklyScore = 0;
-        this.wins = 0;
-        this.losses = 0;
-        this.pushes = 0;
+        this.weeklyWins = 0;
+        this.weeklyLosses = 0;
+        this.weeklyPushes = 0;
     }
 
     // Used by Jackson when a player enters picks (calls setters for other fields)
     public WeekOfPicks() {
         this.weeklyScore = 0;
-        this.wins = 0;
-        this.losses = 0;
-        this.pushes = 0;
+        this.weeklyWins = 0;
+        this.weeklyLosses = 0;
+        this.weeklyPushes = 0;
     }
 
     public void calculateWeeklyScore(WeekOfLines weekOfLines) {
         weeklyScore = 0;
-        wins = 0;
-        losses = 0;
-        pushes = 0;
+        weeklyWins = 0;
+        weeklyLosses = 0;
+        weeklyPushes = 0;
         picks.forEach(pick -> {
             GameLine gameLine = weekOfLines.getLinesOfTheWeek().get(pick.getGameId() - 1);
             if (gameLine.getHomeTeamScore() != null) {
@@ -51,11 +51,11 @@ public class WeekOfPicks {
                 float pointsAwardedForPick = pick.getPointsAwarded(gameLine);
                 weeklyScore += pointsAwardedForPick;
                 if (pointsAwardedForPick == 1.0) {
-                    wins++;
+                    weeklyWins++;
                 } else if (pointsAwardedForPick == 0.0) {
-                    losses++;
+                    weeklyLosses++;
                 } else if (pointsAwardedForPick == 0.5) {
-                    pushes++;
+                    weeklyPushes++;
                 }
             }
         });
